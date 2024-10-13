37ef09742753a1a553c0d6d44d4f6ffb90bc156c
DjKesu <km67000@yahoo.com>
2024-10-13 00:23:34 -0400

    changed to home to render directly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,105 @@
-import Image from "next/image";
+'use client'
+import React, { useCallback, useMemo, useState } from "react"
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+// Player type
+type Player = 'X' | 'O' | null;
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+// Board type
+type Board = Player[];
+
+// winner meta information interface
+interface WinningInfo {
+  winner: Player;
+  line: number[] | null;
+};
+
+function getWinnerAndLine(board:Board): WinningInfo {
+  for(const[a,b,c] of winningCombinations) {
+    if(board[a] && board[a] == board[b] && board[a] == board[c]) // same character, and all three required for winning combination exist on the board
+      return {winner:board[a], line:[a,b,c]};
+  }
+  return {winner:null, line:null};
 }
+
+const winningCombinations = [
+  // rows
+  [0,1,2], [3,4,5],[6,7,8],
+  // columns
+  [0,3,6], [1,4,7], [2,5,8],
+  // diagnols
+  [0,4,8], [2,4,6]
+];
+
+// each square can be clicked, can be null/player/winning
+interface SquareProps {
+  value: Player;
+  onClick: () => void;
+  isWinning: boolean;
+}
+
+const Square: React.FC<SquareProps> = React.memo(({value, onClick, isWinning}) => {
+  // each square is a button with reactive settings
+  return (
+    <button className={`text-center font-bold w-20 h-20 flex items-center justify-center rounded ${isWinning
+      ? 'text-white bg-green-700' : value ? 'text-white bg-[#C9C9C9]' : 'text-white bg-white hover:bg-[#E5E7EB]'
+    }`} onClick={onClick}>{value}</button>
+  );
+});
+
+const Home : React.FC = () => {
+
+  const [board, setBoard] = useState<Board>(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState<Boolean>(true); // X is true, and O is false
+  const {winner, line} = useMemo(() => getWinnerAndLine(board), [board]);
+
+  // display current player and final status
+
+  const status = useMemo(()=> {
+    if(winner) return `${winner} wins!`
+    if(board.every(Boolean)) return "It's a draw!";
+
+    return `${xIsNext ? 'X' : 'O'}'s turn.`
+  }, [winner, board, xIsNext]);
+
+  // reset game
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setXIsNext(true);
+  }
+
+  const handleClick = useCallback((index: number) => {
+    if(winner || board[index])
+      return;
+
+    // update board
+    setBoard(prevBoard => {
+      const newBoard = [...prevBoard];
+      newBoard[index] = xIsNext ? 'X' : 'O'
+      return newBoard;
+    });
+
+    // update player
+    setXIsNext(prevXIsNext => !prevXIsNext);
+
+  }, [winner, board, xIsNext]);
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#111111] ">
+      <h1 className="mb-4">Tic Tac Toe</h1>
+      <div className="grid grid-cols-3 gap-2 flex items-center justify-center mb-4">
+        {board.map((square, index) => (
+            <Square
+              key={index}
+              value={square}
+              onClick={() => handleClick(index)}
+              isWinning = {line ? line.includes(index) : false}
+            />
+          ))}
+      </div>
+      <p className="mb-4">{status}</p>
+      <button className="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-[#98EE98]-600 dark:hover:bg-green-700 " onClick={resetGame}>New Game</button>
+    </div>
+  )
+};
+
+export default Home;
